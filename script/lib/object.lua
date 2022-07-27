7ca4e743c880fcac2727ffaa892c9e11cff3fa86
codewdy <codewdy@gmail.com>
2022-07-27 21:16:16 +0800

    fix codestyle

--- script/lib/object.lua
+++ script/lib/object.lua
@@ -1,69 +1,73 @@
 local M = {}
 
-M.Object = { fields = { }, handle_manager = { pool = { }, ptr = 0 } }
+M.Object = {
+  handle_manager = { pool = { }, ptr = 0 },
 
-function M.Object.init_class(cls)
-  cls.meta = {
-    pool = {},
-    pool_ptr = 0
-  }
-end
+  init_class = function(cls)
+    cls.meta = {
+      pool = {},
+      pool_ptr = 0
+    }
+  end,
 
-function M.Object.new(cls)
-  local result = { class = cls }
-  setmetatable(result, { __index = cls.fields })
-  result:init()
-  return result
-end
-
-function M.Object.create(cls)
-  if cls.meta.pool_ptr == 0 then
-    cls.handle_manager.ptr = cls.handle_manager.ptr + 1
-    local result = cls:new()
-    result.handle = cls.handle_manager.ptr
-    cls.handle_manager.pool[result.handle] = result
+  new = function(cls)
+    local result = { class = cls }
+    setmetatable(result, { __index = cls.fields })
+    result:init()
     return result
-  else
-    local x = cls.meta.pool[cls.meta.pool_ptr]
-    cls.meta.pool_ptr = cls.meta.pool_ptr - 1
-    return x
-  end
-end
+  end,
 
-function M.Object.release(cls, obj)
-  obj:clear()
-  cls.meta.pool_ptr = cls.meta.pool_ptr + 1
-  cls.meta.pool[cls.meta.pool_ptr] = obj
-end
+  create = function(cls)
+    if cls.meta.pool_ptr == 0 then
+      cls.handle_manager.ptr = cls.handle_manager.ptr + 1
+      local result = cls:new()
+      result.handle = cls.handle_manager.ptr
+      cls.handle_manager.pool[result.handle] = result
+      return result
+    else
+      local x = cls.meta.pool[cls.meta.pool_ptr]
+      cls.meta.pool_ptr = cls.meta.pool_ptr - 1
+      return x
+    end
+  end,
 
-function M.Object.inherit(cls, fields)
-  local result = {}
-  for k, v in pairs(cls) do
-    result[k] = v
-  end
-  result.fields = {}
-  for k, v in pairs(cls.fields) do
-    result.fields[k] = v
-  end
-  for k, v in pairs(fields) do
-    result.fields[k] = v
-  end
-  result:init_class()
-  return result
-end
+  release = function(cls, obj)
+    obj:clear()
+    cls.meta.pool_ptr = cls.meta.pool_ptr + 1
+    cls.meta.pool[cls.meta.pool_ptr] = obj
+  end,
 
-function M.Object.get_by_handle(cls, handle)
-  return cls.handle_manager.pool[handle]
-end
+  inherit = function(cls, fields)
+    local result = {}
+    for k, v in pairs(cls) do
+      result[k] = v
+    end
+    result.fields = {}
+    for k, v in pairs(cls.fields) do
+      result.fields[k] = v
+    end
+    for k, v in pairs(fields) do
+      result.fields[k] = v
+    end
+    result:init_class()
+    return result
+  end,
 
-function M.Object.fields.init(self)
-end
+  get_by_handle = function(cls, handle)
+    return cls.handle_manager.pool[handle]
+  end,
 
-function M.Object.fields.clear(self)
-end
+  fields = {
+    init = function(self)
+    end,
 
-function M.Object.fields.release(self)
-  self.class:release(self)
-end
+    clear = function(self)
+    end,
+
+    release = function(self)
+      self.class:release(self)
+    end,
+  },
+}
 
 return M
