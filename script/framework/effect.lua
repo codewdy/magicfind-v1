22c36e6ee85f9dee44d44ba0e4df4f0264950ad0
codewdy <codewdy@gmail.com>
2022-08-03 22:57:26 +0800

    add json

--- script/framework/effect.lua
+++ script/framework/effect.lua
@@ -4,10 +4,11 @@ local Object = require("lib.object").Object
 
 M.EffectPrototype = Object:extend({
   init = function(self)
+    self.dict = {}
   end,
   set_value = function(self, dict)
     for k,v in pairs(dict) do
-      self[k] = v
+      self.dict[k] = v
     end
   end
 })
--- script/framework/unit.lua
+++ script/framework/unit.lua
@@ -10,10 +10,11 @@ M.UnitGroup = Enum({
 
 M.UnitPrototype = Object:extend({
   init = function(self)
+    self.dict = {}
   end,
   set_value = function(self, dict)
     for k,v in pairs(dict) do
-      self[k] = v
+      self.dict[k] = v
     end
   end
 })
